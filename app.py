9efb3d7b94392eba92e733fd8a73cd8279d299eb
Shubham Dobariya <shubhamdobariyas880@gmail.com>
2025-05-19 13:18:03 +0530

    Add API key for poser

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ movies = pickle.load(open('model/movie_list.pkl','rb'))
 similarity = pickle.load(open('model/similarity.pkl','rb'))
 
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=51bf311d05b44831ca3b250002476a9b&language=en-US".format(movie_id)
     data = requests.get(url)
     data = data.json()
     poster_path = data['poster_path']
